ba23406e3b2c746ce3c6e09ac88b1009db12f565
EzioHe <yufeihe64@gmail.com>
2026-04-14 00:54:29 +0800

    已完成项目初始化、用户模块以及图片模块

---
+++ src/access.ts
@@ -0,0 +1,29 @@
+import router from '@/router'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+import { message } from 'ant-design-vue'
+
+let firstFetchLoginUser = true
+
+/**
+ * 全局权限校验，每次切换时都会执行
+ */
+router.beforeEach(async (to, from, next) => {
+  const loginUserStore = useLoginUserStore()
+  let loginUser = loginUserStore.loginUser
+  //确保页面刷新时，首次加载时，能等待后端返回用户信息后再校验权限
+  if (firstFetchLoginUser) {
+    await loginUserStore.fetchLoginUser()
+    loginUser = loginUserStore.loginUser
+    firstFetchLoginUser = false
+  }
+  const toUrl = to.fullPath
+  //自定义权限校验规则，比如管理员才可以访问 /admin 的页面
+  if (toUrl.startsWith('/admin')) {
+    if (!loginUser || loginUser.userRole !== 'admin') {
+      message.error('无权限访问')
+      next(`/user/login?redirect=${to.fullPath}`)
+      return
+    }
+  }
+  next()
+})
---
+++ src/stores/useLoginUserStore.ts
@@ -0,0 +1,30 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import { getLoginUserUsingGet } from '@/api/userController.ts'
+
+export const useLoginUserStore = defineStore('loginUser', () => {
+  const loginUser = ref<API.LoginUserVO>({
+    userName: '未登录',
+  })
+
+  /**
+   * 远程获取登录用户信息
+   */
+  async function fetchLoginUser() {
+    const res = await getLoginUserUsingGet()
+    if (res.data.code === 0 && res.data.data) {
+      loginUser.value = res.data.data
+    }
+
+    // //测试用户登录，3秒自动登录
+    // setTimeout(() => {
+    //   loginUser.value = { userName: '测试用户', id: 1 }
+    // }, 3000)
+  }
+
+  function setLoginUser(newLoginUser: any) {
+    loginUser.value = newLoginUser
+  }
+
+  return { loginUser, fetchLoginUser, setLoginUser }
+})
